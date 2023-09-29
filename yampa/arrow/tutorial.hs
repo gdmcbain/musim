bed20e45250b352258fb6137033a7d28dbb74125
G. D. McBain <gdmcbain@pm.me>
2023-09-29 16:48:50 +1000

    Arrow tutorial

--- yampa/arrow/tutorial.hs
+++ yampa/arrow/tutorial.hs
@@ -5,7 +5,7 @@ import Control.Monad
 import qualified Control.Category as Cat
 import Data.List
 import Data.Maybe
-import System.Random
+-- import System.Random
 
 newtype Circuit a b = Circuit { unCircuit :: a -> (Circuit a b, b) }
 
@@ -22,6 +22,22 @@ instance Arrow Circuit where
     arr f = Circuit $ \a -> (arr f, f a)
     first (Circuit cir) = Circuit $ \(b, d) ->
         let (cir', c) = cir b
-        in (first cir' (c, d))
+        in (first cir', (c, d))
 
-main = putStrLn "Nothing to see here."
+runCircuit :: Circuit a b -> [a] -> [b]
+runCircuit cir = snd . mapAccumL unCircuit cir
+
+-- | Accumulator that outputs a value determined by the supplied function.
+accum :: acc -> (a -> acc -> (b, acc)) -> Circuit a b
+accum acc f = Circuit $ \input ->
+    let (output, acc') = input `f` acc
+    in  (accum acc' f, output)
+
+-- | Accumulator that outputs the accumulator value.
+accum' :: b -> (a -> b -> b) -> Circuit a b
+accum' acc f = accum acc (\a b -> let b' = a `f` b in (b', b'))
+
+total :: Num a => Circuit a a
+total = accum' 0 (+)
+
+main = putStrLn $ show $ runCircuit total [1,0,1,0.0,2]
